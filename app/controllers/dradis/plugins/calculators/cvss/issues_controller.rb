32d1d7e617d41adb6247d5d995cd45815aa3d8ce
Daniel Martin <etd-github@nomejortu.com>
2019-02-20 16:07:20 +0100

    Issues#edit - load form values from existing record

--- app/controllers/dradis/plugins/calculators/cvss/issues_controller.rb
+++ app/controllers/dradis/plugins/calculators/cvss/issues_controller.rb
@@ -1,13 +1,31 @@
 module Dradis::Plugins::Calculators::CVSS
   # Does it matter that we're inheriting from the no-frills controller?
   class IssuesController < ::IssuesController
+    before_action :set_cvss_vector, only: :edit
 
     def edit
-      render layout: 'snowcrash'
     end
 
     def update
-      render text: '#update'
+      cvss_fields = Hash[ *params[:cvss_fields].scan(HasFields::REGEX).flatten.map(&:strip) ]
+      cvss_fields.each do |name, value|
+        @issue.set_field(name, value)
+      end
+
+      if @issue.save
+        redirect_to main_app.project_issue_path(current_project, @issue), notice: 'CVSSv3 fields updated.'
+      else
+        render :edit
+      end
+    end
+
+    def set_cvss_vector
+      # Undefined Temporal and Environmental default to X
+      @cvss_vector = Hash.new { |h, k| h[k] = 'X' }
+      field_value  = @issue.fields['CVSSv3.Vector'] || @issue.fields['CVSSv3Vector']
+      if field_value
+        field_value.split('/').each { |pair| @cvss_vector.store *pair.split(':') }
+      end
     end
   end
 end

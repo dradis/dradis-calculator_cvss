948970f74b15dbd4b929d857050a3c8c92bda92e
Sean Yeoh <sean.yeoh@hotmail.com>
2021-01-25 15:22:39 +0800

    Add tylium.js and fix typo for application.css

--- lib/dradis/plugins/calculators/cvss/engine.rb
+++ lib/dradis/plugins/calculators/cvss/engine.rb
@@ -9,7 +9,8 @@ module Dradis::Plugins::Calculators::CVSS
     initializer 'calculator_cvss.asset_precompile_paths' do |app|
       app.config.assets.precompile += [
         'dradis/plugins/calculators/cvss/manifests/application.js',
-        'dradis/plugins/calculators/cvss/manifests/application.cs'
+        'dradis/plugins/calculators/cvss/manifests/tylium.js',
+        'dradis/plugins/calculators/cvss/manifests/application.css'
       ]
     end
 

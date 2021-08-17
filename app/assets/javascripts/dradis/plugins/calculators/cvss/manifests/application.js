157c164ec88efd1d6b24090438c845f44169f5e3
Matt Budz <matt.budziosz@gmail.com>
2021-08-17 13:24:21 +0200

    add `cvsscalc30_helptext.js`

--- app/assets/javascripts/dradis/plugins/calculators/cvss/manifests/application.js
+++ app/assets/javascripts/dradis/plugins/calculators/cvss/manifests/application.js
@@ -4,4 +4,5 @@
 //= require turbolinks
 
 //= require dradis/plugins/calculators/cvss/vendor/cvsscalc30
+//= require dradis/plugins/calculators/cvss/vendor/cvsscalc30_helptext
 //= require dradis/plugins/calculators/cvss/calculator

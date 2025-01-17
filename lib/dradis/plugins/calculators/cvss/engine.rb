dd40d5427b97a62710bd4d254c5f7548902b6433
Caitlin <caitlinhenry.ss@gmail.com>
2025-01-17 12:33:54 -0500

    remove db & configuration table check as it is now done in dradis-plugins

--- lib/dradis/plugins/calculators/cvss/engine.rb
+++ lib/dradis/plugins/calculators/cvss/engine.rb
@@ -26,13 +26,11 @@ module Dradis::Plugins::Calculators::CVSS
       # initialization, we first check if the DB is loaded and the Configuration
       # table has been created, before checking if the engine is enabled
       Rails.application.reloader.to_prepare do
-        if (ActiveRecord::Base.connection rescue false) && ::Configuration.table_exists?
-          Rails.application.routes.append do
-            # Enabling/disabling integrations calls Rails.application.reload_routes! we need the enable
-            # check inside the block to ensure the routes can be re-enabled without a server restart
-            if Engine.enabled?
-              mount Engine => '/', as: :cvss_calculator
-            end
+        Rails.application.routes.append do
+          # Enabling/disabling integrations calls Rails.application.reload_routes! we need the enable
+          # check inside the block to ensure the routes can be re-enabled without a server restart
+          if Engine.enabled?
+            mount Engine => '/', as: :cvss_calculator
           end
         end
       end

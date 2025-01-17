552aa83219a00eb01a8a0106256270d91e313b70
Caitlin <caitlinhenry.ss@gmail.com>
2025-01-17 12:43:44 -0500

    remove outdated comment

--- lib/dradis/plugins/calculators/cvss/engine.rb
+++ lib/dradis/plugins/calculators/cvss/engine.rb
@@ -22,9 +22,6 @@ module Dradis::Plugins::Calculators::CVSS
     end
 
     initializer 'calculator_cvss.mount_engine' do
-      # By default, this engine is loaded into the main app. So, upon app
-      # initialization, we first check if the DB is loaded and the Configuration
-      # table has been created, before checking if the engine is enabled
       Rails.application.reloader.to_prepare do
         Rails.application.routes.append do
           # Enabling/disabling integrations calls Rails.application.reload_routes! we need the enable

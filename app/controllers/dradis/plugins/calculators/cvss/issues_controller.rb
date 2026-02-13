b9b161d52042d59d3a27b73f3bef1f31dddc20e8
MattBudz <matt.budz@protonmail.com>
2026-02-13 18:02:39 +0100

    account for existing fields when rendering switches + remove fields when switched off
    also added a small styling consistency change

--- app/controllers/dradis/plugins/calculators/cvss/issues_controller.rb
+++ app/controllers/dradis/plugins/calculators/cvss/issues_controller.rb
@@ -9,9 +9,14 @@ module Dradis::Plugins::Calculators::CVSS
     def edit; end
 
     def update
-      cvss_fields = Hash[ *params[:cvss_fields].scan(FieldParser::FIELDS_REGEX).flatten.map(&:strip) ]
+      cvss_fields = Hash[*params[:cvss_fields].scan(FieldParser::FIELDS_REGEX).flatten.map(&:strip)]
       cvss_fields.each do |name, value|
         @issue.set_field(name, value)
+      end
+
+      existing_v4_fields = @issue.fields.keys.select { |k| V4::FIELDS.include?(k) }
+      (existing_v4_fields - cvss_fields.keys).each do |name|
+        @issue.delete_field(name)
       end
 
       if @issue.save
@@ -28,34 +33,35 @@ module Dradis::Plugins::Calculators::CVSS
       @cvss3_vector = Hash.new { |h, k| h[k] = 'X' }
       @cvss4_vector = Dradis::Plugins::Calculators::CVSS::V4::DEFAULT_CVSS_V4.clone
       field_value_v3 = @issue.fields['CVSSv3.Vector'] || @issue.fields['CVSSv3Vector']
-      field_value_v4  = @issue.fields['CVSSv4.BaseVector']
+      field_value_v4 = @issue.fields['CVSSv4.BaseVector']
 
       # If no vector is set yet, that's OK
       return if field_value_v3.blank? && field_value_v4.blank?
 
       if field_value_v3
         if field_value_v3 =~ V3::VECTOR_REGEXP
-          field_value_v3.split('/').each { |pair| @cvss3_vector.store *pair.split(':') }
+          field_value_v3.split('/').each { |pair| @cvss3_vector.store(*pair.split(':')) }
         else
-          redirect_to main_app.project_issue_path(current_project, @issue), alert: 'The format of the CVSSv3 Vector field is invalid.'
+          redirect_to main_app.project_issue_path(current_project, @issue),
+                      alert: 'The format of the CVSSv3 Vector field is invalid.'
         end
       end
 
-      if field_value_v4
-        if field_value_v4.starts_with?('CVSS:4.0')
-          field_value_v4.split('/').each { |pair| @cvss4_vector.store *pair.split(':') }
-        else
-          redirect_to main_app.project_issue_path(current_project, @issue), alert: 'The format of the CVSSv4 Vector field is invalid.'
-        end
+      return unless field_value_v4
+
+      if field_value_v4.starts_with?('CVSS:4.0')
+        field_value_v4.split('/').each { |pair| @cvss4_vector.store(*pair.split(':')) }
+      else
+        redirect_to main_app.project_issue_path(current_project, @issue),
+                    alert: 'The format of the CVSSv4 Vector field is invalid.'
       end
     end
 
     def set_cvss_version
       @cvss_version =
-        case
-        when @issue.fields['CVSSv3.Vector']&.include?('CVSS:3.1')
+        if @issue.fields['CVSSv3.Vector']&.include?('CVSS:3.1')
           '3.1'
-        when @issue.fields['CVSSv3.Vector']&.include?('CVSS:3.0')
+        elsif @issue.fields['CVSSv3.Vector']&.include?('CVSS:3.0')
           '3.0'
         else
           '4.0'
